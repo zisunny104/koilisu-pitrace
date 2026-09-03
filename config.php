f619dc8e2e2d1d5482b3032271f756ddbf70b7be
zisunny104 <zisunny104@gmail.com>
2026-09-03 08:50:08 +0800

    Add tags for the koilisu home page listing

--- config.php
+++ config.php
@@ -3,5 +3,6 @@ return [
     'name' => 'Pitrace | 拾印',
     'description' => '手繪／掃描素材去背、校正、透明化並個別輸出的圖形化工具',
     'version' => '0.1.1',
-    'author' => 'Tokas(Xiang-zi Xie)'
+    'author' => 'Tokas(Xiang-zi Xie)',
+    'tags' => ['圖像', '去背']
 ];
